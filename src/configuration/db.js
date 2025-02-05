20711951bd9db37362001d96db9f835f5181f349
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-02-04 22:09:54 -0300

    feat: adicionada a sincronização do banco de dados em caso de nova instância do banco, assim como uma função que cria o banco, caso o mesmo não exista. adicionadas as variáveis de ambiente que faltavam no env.js

--- src/configuration/db.js
+++ src/configuration/db.js
@@ -4,9 +4,34 @@ import ENV from "./env.js";
 const uri = ENV.POSTGRES_URI;
 const sequelize = new Sequelize(uri);
 
+async function createDatabase() {
+    try {
+        const tempSequelize = new Sequelize(
+            `postgres://${ENV.POSTGRES_USER}:${ENV.POSTGRES_PASSWORD}@localhost:5432`
+        );
+
+        await tempSequelize.authenticate();
+
+        const dbName = ENV.POSTGRES_DATABASE;
+        const [result] = await tempSequelize.query(
+            `SELECT 1 FROM pg_database WHERE datname = '${dbName}'`
+        );
+
+        if (result.length === 0) {
+            await tempSequelize.query(`CREATE DATABASE ${dbName}`);
+        }
+
+        await tempSequelize.close();
+    } catch (error) {
+        console.error("Erro ao criar banco de dados: ", error);
+    }
+}
+
 async function connectToPostgres() {
     try {
+        await createDatabase();
         await sequelize.authenticate();
+        await sequelize.sync({ force: false });
         console.log("Conexão com o Postgres estabelecida com sucesso.");
     } catch (error) {
         console.error("Conexão com o banco falhou.", error);
--- src/configuration/env.js
+++ src/configuration/env.js
@@ -4,9 +4,12 @@ dotenv.config();
 
 const ENV = {
     POSTGRES_URI: process.env.POSTGRES_URI || "",
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE || "",
+    POSTGRES_USER: process.env.POSTGRES_USER || "",
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || "",
 };
 
-if (!ENV.POSTGRES_URI) {
+if (!ENV.POSTGRES_URI || !ENV.POSTGRES_DATABASE) {
     throw new Error("Variáveis do ambiente não encontradas.");
 }
 

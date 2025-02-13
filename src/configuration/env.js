4fc4d54fe2a82d70b998857a09002da3eb221e2e
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-02-13 09:38:55 -0300

    fix: adicionada checagem caso user e password, variáveis de ambiente para o banco de dados, estejam vazias. adicionada network própria para o contêiner do banco de dados utilizando o driver bridge, por algum motivo o padrão modificou esse driver e a conexão com o banco havia quebrado.

--- src/configuration/env.js
+++ src/configuration/env.js
@@ -9,7 +9,7 @@ const ENV = {
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || "",
 };
 
-if (!ENV.POSTGRES_URI || !ENV.POSTGRES_DATABASE || !POSTGRES_USER || !POSTGRES_PASSWORD) {
+if (!ENV.POSTGRES_URI || !ENV.POSTGRES_DATABASE || !ENV.POSTGRES_USER || !ENV.POSTGRES_PASSWORD) {
     throw new Error("Variáveis do ambiente não encontradas.");
 }
 
